25d23c74f68f97670b6047185a4208ac10ae6e26
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-14 13:35:10 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan33/userlogin/Latihan33Login.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan33.userlogin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program user login dengan java berbasis object
+ */
+public class Latihan33Login {
+
+    private static String usName, passWord;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        User user = new User();
+        
+        System.out.print("Masukkan Username = ");
+        usName = scanner.next();
+        
+        System.out.print("Masukkan Password = ");
+        passWord = scanner.next();
+        
+        user.pengecekkanLogin(usName, passWord);
+    }
+    
+}
